53ea6a1752efa96bad53cccd31164ec132706f45
Marc Doerflinger <github@doerf.li>
2018-03-31 18:32:55 +0200

    add show method

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -15,6 +15,10 @@ class RecipiesController < ApplicationController
     redirect_to action: :index
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   private
     def recipe_params
       params.require(:recipe).permit(:name, :ingredients, :instructions, :duration)
