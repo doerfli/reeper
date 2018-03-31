3b4fc0c9a50120b4166c382f738de28b1126891e
Marc Doerflinger <github@doerf.li>
2018-03-31 18:15:34 +0200

    create recipe and show list

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -1,6 +1,23 @@
 class RecipiesController < ApplicationController
 
   def index
+    @recipes = Recipe.all
   end
-  
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    recipe.save
+
+    redirect_to action: :index
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :ingredients, :instructions, :duration)
+    end
+
 end
