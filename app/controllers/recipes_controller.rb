6b25a4b1a5a552da4ff4bcb1ec568a1506e02e7a
Marc Doerflinger <github@doerf.li>
2018-03-31 23:42:45 +0200

    rename controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,4 @@
-class RecipiesController < ApplicationController
-
+class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RecipiesControllerTest < ActionDispatch::IntegrationTest
+class RecipesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
