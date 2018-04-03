d317e18f5ad5e3ba0d6cc0bc2120c589f186072e
Marc Doerflinger <github@doerf.li>
2018-04-03 22:18:54 +0200

    add s3 to csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,7 +7,7 @@
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self
   policy.font_src    :self, :data
-  policy.img_src     :self, :data
+  policy.img_src     :self, :data, 'https://reeper.s3.eu-central-1.amazonaws.com'
   policy.object_src  :none
   policy.script_src  :self
   policy.style_src   :self
