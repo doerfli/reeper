d16909d14da45984c25e1b6686b3edeb3ecdc558
Marc Doerflinger <github@doerf.li>
2018-04-07 22:43:32 +0200

    fix

--- app/controllers/recipe_images_controller.rb
+++ app/controllers/recipe_images_controller.rb
@@ -8,8 +8,7 @@ class RecipeImagesController < ApplicationController
 
     if params[:image].nil?
       flash[:error] = t('recipes.images.image_missing')
-      redirect_to new_recipe_recipe_image_path(recipe)
-      return
+      redirect_to new_recipe_recipe_image_path(recipe) and return
     end
 
     recipe.recipe_images.attach(params[:image])
