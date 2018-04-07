22eae725261d42265ca057e0fc2049ec6f247003
Marc Doerflinger <github@doerf.li>
2018-04-07 22:32:33 +0200

    show error when no image is selected

--- app/controllers/recipe_images_controller.rb
+++ app/controllers/recipe_images_controller.rb
@@ -5,6 +5,13 @@ class RecipeImagesController < ApplicationController
 
   def create
     recipe = Recipe.find(params[:recipe_id])
+
+    if params[:image].nil?
+      flash[:error] = t('recipes.images.image_missing')
+      redirect_to new_recipe_recipe_image_path(recipe)
+      return
+    end
+
     recipe.recipe_images.attach(params[:image])
     recipe.save
 
