71b860f12dbb795c1184ff5433bc91708c310ed7
Marc Doerflinger <github@doerf.li>
2018-04-13 00:08:10 +0200

    #21 set tag name unique

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,7 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :recipes
 
+  validates :name, uniqueness: true
+
   scope :match_term, ->(term) { where('name ILIKE ?', "%#{term.downcase}%") }
 end
