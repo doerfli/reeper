bbd3d3769ca3fd13be26f2057206cd4ebf27d56e
Marc Doerflinger <github@doerf.li>
2018-04-28 21:50:33 +0200

    add test

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -19,5 +19,16 @@ RSpec.describe Recipe, type: :model do
       rc.save
       expect(Recipe.count).to eq 0
     end
+    it 'should save comma separted tags' do
+      rc = Recipe.new do |r|
+        r.name = 'rezept mit tags'
+        r.ingredients = 'mehl,wasser,sonstiges'
+        r.instructions = 'zusammenschuetten und backen'
+        r.tag_names = 'mehl, Wasser,ei'
+      end
+      rc.save
+      expect(Recipe.count).to eq 1
+      expect(Tag.count).to eq 3
+    end
   end
 end
