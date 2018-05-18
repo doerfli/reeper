fd4a08070969393a72d1b1054c7c99abe2d6db59
Marc Doerflinger <github@doerf.li>
2018-05-18 07:39:54 +0200

    #54 add create method for ocr

--- app/controllers/ocr_controller.rb
+++ app/controllers/ocr_controller.rb
@@ -2,4 +2,19 @@ class OcrController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def create
+    @recipe = Recipe.find(params[:id])
+    image = @recipe.recipe_images.select{|i| i.id == params[:imgid]}.first
+    tmp = Tempfile.new('for_rt')
+    tmp.write(i.download.force_encoding("UTF-8"))
+    tmp.close
+
+    # TODO crop image to coordinates
+
+    image = RTesseract.new(tmp, processor: 'mini_magick', lang: 'deu')
+    logger.info image.lang
+    logger.info image.to_s
+    tmp.unlink
+  end
 end
