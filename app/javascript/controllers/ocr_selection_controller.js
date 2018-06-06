32675a523824506730323cf6f3090d6db0bcf819
Marc Doerflinger <github@doerf.li>
2018-06-06 23:27:14 +0200

    #54 draw box over selected region

--- app/javascript/controllers/ocr_selection_controller.js
+++ app/javascript/controllers/ocr_selection_controller.js
@@ -2,12 +2,49 @@ import { Controller } from "stimulus"
 import { Utils } from "../src/utils"
 
 export default class extends Controller {
-  static targets = [ "filenames", "submitbutton", "x1", "x2", "y1", "y2", "recognizedtext"]
+  static targets = [ "filenames", "submitbutton", "x1", "x2", "y1", "y2", "recognizedtext", "canvas"]
+
+  initialize() {
+    // initialize canvas size
+    let canvas = this.canvasTarget;
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+  
+    this.drawImageToCanvas(this.data.get("imgurl"));
+  }
+
+  drawImageToCanvas(url) {
+    var thisdata = this.data;
+    var canvas = this.canvasTarget;
+    var context = canvas.getContext('2d');
+    
+    var drawing = new Image();
+    drawing.src = url;
+    drawing.onload = function() {
+      // get canvas width
+      var width = canvas.clientWidth;
+      // calculate height relative to canvas width
+      var ratio = width / drawing.width;
+      var height = ratio * drawing.height;
+
+      if ( height > canvas.clientHeight) {
+        // if height > canvas height -> resize height to canvas height and width relative to height
+        height = canvas.clientHeight;
+        ratio = height / drawing.height;
+        width = ratio * drawing.width;
+      }
+
+      thisdata.set("ratio", ratio);
+      context.drawImage(drawing,0,0, width, height);
+    };
+  }
 
   select(event) {
-    var filename = event.target.dataset.imgid;
-    console.log(filename);
-    // TODO upate imgid and image
+    var id = event.target.dataset.imgid;
+    var url = event.target.dataset.imgurl;
+    this.data.set("imgid", id);
+    this.data.set("imgurl", url);
+    this.initialize(url);
   }
 
   mousedown(event) {
@@ -28,6 +65,7 @@ export default class extends Controller {
     }
     console.log("mousemove");
     console.log(event);
+    var ratio = _.toNumber(this.data.get("ratio"));
     var x = event.pageX - event.target.offsetParent.offsetLeft - event.target.offsetLeft;
     var y = event.pageY - event.target.offsetParent.offsetTop - event.target.offsetTop;
     this.x2Target.innerHTML = _.toString(x);
@@ -44,18 +82,29 @@ export default class extends Controller {
     this.x2Target.innerHTML = _.toString(x);
     this.y2Target.innerHTML = _.toString(y);
     // TODO show selected area on image
+
+    let canvas = this.canvasTarget;
+    let context = canvas.getContext('2d');
+    let x1 = _.toNumber(this.x1Target.innerHTML);
+    let y1 = _.toNumber(this.y1Target.innerHTML);
+    
+    context.fillStyle = "blue";
+    context.globalAlpha = 0.3;
+    context.fillRect(x1, y1, x - x1, y - y1);
+    context.globalAlpha = 1.0;
   }
 
   submit(event) {
     // TODO selection
     console.log(event);
-    var url = this.data.get("url");
+    var url = this.data.get("ocrurl");
     var id = this.data.get("id");
     var imgid = this.data.get("imgid");
-    var x1 = this.x1Target.innerHTML;
-    var y1 = this.y1Target.innerHTML;
-    var x2 = this.x2Target.innerHTML;
-    var y2 = this.y2Target.innerHTML;
+    var ratio = _.toNumber(this.data.get("ratio"));
+    var x1 = _.toNumber(this.x1Target.innerHTML) / ratio;
+    var y1 = _.toNumber(this.y1Target.innerHTML) / ratio;
+    var x2 = _.toNumber(this.x2Target.innerHTML) / ratio;
+    var y2 = _.toNumber(this.y2Target.innerHTML) / ratio;
     var textArea = this.recognizedtextTarget;
     
     // TODO show spinner
