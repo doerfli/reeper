892f650eb239c797329f2152ff8b24e553d39b3a
github@doerf.li <marc@doerf.li>
2021-09-29 19:47:53 +0000

    stimulus 3.0.0 fixes

--- app/javascript/controllers/recipe_controller.js
+++ app/javascript/controllers/recipe_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "stimulus"
+import { Controller } from "@hotwired/stimulus"
 import { Utils } from "../src/utils"
 
 export default class extends Controller {
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,8 @@
 
 import "./styles.scss";
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
