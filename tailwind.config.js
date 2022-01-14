143ac1fe8e968fc2439d859f9386f34a410f1b54
github@doerf.li <marc@doerf.li>
2022-01-14 19:42:02 +0000

    colors (#64)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,26 @@ module.exports = {
       './app/helpers/**/*.rb',
       './app/javascript/**/*.js'
     ],
+    theme: {
+      extend: {
+        typography: (theme) => ({
+          DEFAULT: {
+            css: {
+              lineHeight: '1.1',
+              a: {
+                textDecoration: 'none',
+                padding: '0.25rem',
+                borderRadius: '0.375rem',
+              },
+              'a:hover': {
+                color: theme('colors.indigo.800'),
+                backgroundColor: theme('colors.blue.200'),
+              },
+            },
+          },
+        }),
+      },
+    },
     plugins: [
       require('@tailwindcss/typography'),
     ],
