e051d9e35d176af2c702930c374e65e5145f632d
doerfli <doerfli@users.noreply.github.com>
2022-10-10 19:25:47 +0000

    show spinner when sending file (closes #224)

--- app/javascript/controllers/recipeimagesadd_controller.js
+++ app/javascript/controllers/recipeimagesadd_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "files", "buttons", "template" ]
+  static targets = [ "files", "buttons", "template", "saveButton", "spinner", "form" ]
 
   add_another() {
     var t = this.templateTarget.cloneNode(true);
@@ -9,5 +9,16 @@ export default class extends Controller {
     t.classList.remove("hidden");
     t.removeAttribute("data-recipeimagesadd-target");
     this.filesTarget.insertBefore(t, this.buttonsTarget);
+  }
+
+  save(event) {
+    console.log(event);
+    event.preventDefault();
+    this.saveButtonTarget.disabled = true;
+    const altText = this.saveButtonTarget.dataset.alttext;
+    this.saveButtonTarget.innerHTML = altText;
+    this.spinnerTarget.classList.remove("hidden");
+    console.log("save");
+    this.formTarget.submit();
   }
 }
